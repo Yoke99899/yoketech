75309551f4c1429fb8c00455bb788cdf1ee7f2cb
Yoke99899 <yoke.septian7@gmail.com>
2025-06-26 21:20:26 +0700

    Add files via upload

---
+++ src/firebaseClient.js
@@ -0,0 +1,45 @@
+// src/firebaseClient.js
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, get, set, update, remove, onValue } from 'firebase/database';
+import {
+  getStorage,
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL
+} from 'firebase/storage';
+
+// Konfigurasi Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyApDvF3Y59XfO6w4mNDr44DkaO7FKe9jho",
+  authDomain: "frontend-spg.firebaseapp.com",
+  databaseURL: "https://frontend-spg-default-rtdb.firebaseio.com",
+  projectId: "frontend-spg",
+  storageBucket: "frontend-spg.appspot.com",
+  messagingSenderId: "988189347575",
+  appId: "1:988189347575:web:21ecbffc732315a2244df2",
+  measurementId: "G-0J7DVZDGZ2"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage(app);
+
+// Akses global (opsional)
+if (typeof window !== "undefined") {
+  window.firebaseDB = db;
+}
+
+export {
+  db,
+  ref,
+  get,
+  set,
+  update,
+  remove,
+  onValue,
+  storage,
+  storageRef,
+  uploadBytes,
+  getDownloadURL
+};
