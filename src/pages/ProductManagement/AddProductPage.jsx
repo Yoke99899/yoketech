3bd1f600d574dca528383aeab8e4449258dc4846
Yoke99899 <yoke.septian7@gmail.com>
2025-07-27 05:54:50 +0700

    Update AddProductPage.jsx

--- src/pages/ProductManagement/AddProductPage.jsx
+++ src/pages/ProductManagement/AddProductPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ProductForm from './ProductForm';
-import useProductPage from './useProductPage';
+import UseProductPage from './UseProductPage';
 
 export default function AddProductPage({ onClose }) {
-  const { addProduct } = useProductPage();
+  const { addProduct } = UseProductPage();
 
   const handleSubmit = async (data) => {
     await addProduct(data);
