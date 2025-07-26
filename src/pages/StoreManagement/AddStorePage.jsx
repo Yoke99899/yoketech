c0ee3ddf10744028d892e51b43257d894f52de9c
Yoke99899 <yoke.septian7@gmail.com>
2025-07-27 05:52:50 +0700

    Update AddStorePage.jsx

--- src/pages/StoreManagement/AddStorePage.jsx
+++ src/pages/StoreManagement/AddStorePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import StoreForm from './StoreForm';
-import useStorePage from './useStorePage';
+import useStorePage from './UseStorePage';
 
 export default function AddStorePage({ onClose }) {
   const { addStore } = useStorePage();
